294ca239b6179f193ddc0ab1b68793fb8de6822e
Jsowmya08 <sowmya.jangam0807@gmail.com>
2021-10-09 00:39:45 -0700

    wages for month

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Wages_for_month
+{
+    class Program
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+        public const int empRatePerHour = 20;
+        public const int numOfWorkingDays = 20;
+        public const int maxHrsInMonth = 100;
+        static void Main(string[] args)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= maxHrsInMonth  && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case isPartTime: 
+                        empHrs = 4;
+                        break;
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:" + totalWorkingDays + " Emp Hrs : " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        }
+    }
+}
+
+    
+
